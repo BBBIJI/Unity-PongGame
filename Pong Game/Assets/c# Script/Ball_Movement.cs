d3cf980641b3b6f5582ec143469e8e552bea4413
BBBIJI <liekenny273@gmail.com>
2022-12-27 23:12:38 +0800

    Increasing Speed and Reset Function added

--- Pong Game/Assets/c# Script/Ball_Movement.cs
+++ Pong Game/Assets/c# Script/Ball_Movement.cs
@@ -1,34 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ball_Movement : MonoBehaviour
 {
-    float speed_x = 0.1f;
-    float speed_y = 0.1f;
-    void Update()
-    {
-        transform.Translate(speed_x,speed_y,0);   
+    float speed_x = 3f;
+    float speed_y = 3f;
+
+    void Start(){
+        PauseGame();
     }
 
-void OnCollisionEnter2D(Collision2D collision)
-{
-    if (collision.gameObject.tag == "Up_Wall")
-    {
-        speed_y = -speed_y;
+    void Update(){
+        if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.S)||Input.GetKey("up")||Input.GetKey("down")){
+                    ResumeGame();
+        }
+        transform.Translate(speed_x*Time.deltaTime,speed_y*Time.deltaTime,0);   
+
+        if(speed_x>0){
+            speed_x += 0.01f;
+        }
+        if(speed_y>0){
+            speed_y += 0.01f;
+        }
+        if(speed_x<0){
+            speed_x -= 0.01f;
+        }
+        if(speed_y<0){
+            speed_y -= 0.01f;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+
+        if (collision.gameObject.tag == "Up_Wall")
+        {
+            speed_y = -speed_y;
         }
         if (collision.gameObject.tag == "Low_Wall")
         {
             speed_y = -speed_y;
-            Debug.Log("kontol");
         }
         if (collision.gameObject.tag == "Right_Wall")
         {
-            speed_x = -speed_x;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
         if (collision.gameObject.tag == "Left_Wall")
-    {
+        {
+	        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (collision.gameObject.tag == "Player_Left")
+        {
             speed_x = -speed_x;
         }
+        if (collision.gameObject.tag == "Player_Right")
+        {
+            speed_x = -speed_x;
+        }
+
+    }
+
+    void PauseGame ()
+    {
+        Time.timeScale = 0;
+    }
+    void ResumeGame ()
+    {
+        Time.timeScale = 1;
     }
 }
